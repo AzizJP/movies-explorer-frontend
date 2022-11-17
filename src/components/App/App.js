7fc7ba5b0b018a8bbafcc0c00ccacd58a3ba488b
Aziz <t1-zer@yandex.ru>
2022-11-17 11:46:34 +0300

    feat: close popup when escape is pressed

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,4 +1,4 @@
-import { memo, useCallback, useState } from 'react';
+import { memo, useCallback, useEffect, useState } from 'react';
 import { Switch } from 'react-router-dom';
 import Header from '../Header/Header';
 import Sidebar from '../Sidebar/Sidebar';
@@ -6,6 +6,19 @@ import './App.css';
 
 const App = memo(() => {
   const [isOpenMenu, setIsOpenMenu] = useState(false);
+
+  useEffect(() => {
+    const closeByEscape = evt => {
+      if (evt.key === 'Escape') {
+        setIsOpenMenu(false);
+      }
+    };
+    if (isOpenMenu) {
+      document.addEventListener('keydown', closeByEscape);
+    } else {
+      document.removeEventListener('keydown', closeByEscape);
+    }
+  }, [isOpenMenu]);
 
   const handleMenuButtonClick = useCallback(() => {
     setIsOpenMenu(true);
