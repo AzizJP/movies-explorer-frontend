12be170aa0130fa3843d68d66d6da5850dd965c9
Aziz <t1-zer@yandex.ru>
2022-11-23 10:04:48 +0300

    feat: add about me section

--- src/components/Main/Portfolio/Portfolio.jsx
+++ src/components/Main/Portfolio/Portfolio.jsx
@@ -1,9 +1,6 @@
 import { memo } from 'react';
-import {
-  getWordEnding,
-  getYearsOld,
-} from '../../Shared/Functions/Functions';
 
+import AboutMe from '../AboutMe/AboutMe';
 import { ReactComponent as Arrow } from '../../../images/arrow.svg';
 
 import './Portfolio.css';
@@ -27,35 +24,7 @@ const Portfolio = memo(() => {
   return (
     <section id="student" className="portfolio__wrapper">
       <h2 className="title">Студент</h2>
-      <div className="portfolio__about-me_wrapper">
-        <img
-          src="https://sun9-30.userapi.com/impf/c841327/v841327980/2550f/oUcGfWcf2-I.jpg?size=1280x812&quality=96&sign=de68715e0265db267d7c72d9bacc1fb7&type=album"
-          alt="Фотография"
-          className="portfolio__image"
-        />
-        <div className="portfolio__about-me">
-          <h3 className="portfolio__about-me_name">Азиз</h3>
-          <h4 className="portfolio__about-me_job">{`Фронтент-разработчик, ${getYearsOld()} ${getWordEnding(
-            getYearsOld()
-          )}`}</h4>
-          <p className="portfolio__about-me_text">
-            Родился в Туркмении, живу и развиваюсь в Санкт-Петербурге.
-            Меня всегда тянуло сделать комфортней свою жизнь и жизнь
-            людей, которые меня окружают. Сначала я направил силы в
-            работу архитектором, а сейчас - фронтэнд-разработчиком...
-          </p>
-          <div>
-            <a
-              href="https://github.com/AzizJP"
-              target="_blank"
-              className="portfolio__about-me_link link-hover"
-              rel="noreferrer"
-            >
-              Github
-            </a>
-          </div>
-        </div>
-      </div>
+      <AboutMe />
       <div className="portfolio__projects">
         <h5 className="portfolio__projects_title">Портфолио</h5>
         {projects.map(({ name, link }) => (
